a5e1dd3014c40fb9263408a944770eb7ea331c25
alexander-erin <a.erin@fkcore.com>
2022-11-09 19:35:26 +0300

    add todo

--- src/Dto/Input/Pagination.php
+++ src/Dto/Input/Pagination.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Symfony\PresentationBundle\Dto\Input;
 
+//todo: сделать это интерфейсом как-нибудь, чтобы была возможность кастомизировать валидацию в конструкторе
 class Pagination
 {
     private int $pageNumber;
@@ -12,6 +13,7 @@ class Pagination
 
     public function __construct(int $pageNumber = 1, int $pageSize = 1, int $maxSize = 500)
     {
+        //todo: если $pageNumber меньше 1 - то присвоить единицу
         $this->pageNumber = $pageNumber;
         $this->pageSize = ($pageSize > $maxSize) ? $maxSize : $pageSize;
         $this->offset = ($this->pageNumber - 1) * $this->pageSize;
--- src/PresentationBundle.php
+++ src/PresentationBundle.php
@@ -6,6 +6,8 @@ namespace Symfony\PresentationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+//todo: добавить возможность опционально выбирать только часть полей для OutputContract
+//todo: добавить возможность изменять поведение на передачу клиентом несуществующих фильтров
 class PresentationBundle extends Bundle
 {
 
