ea8dc884ec308ad92a4788407d34d088e235626a
alexander-erin <a.erin@fkcore.com>
2022-12-11 15:53:10 +0300

    add todo

--- src/PresentationBundle.php
+++ src/PresentationBundle.php
@@ -6,6 +6,8 @@ namespace IWD\Symfony\PresentationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+//todo: добавить модуль авто-тестов для автоматической проверки корректности работы read, search-сценариев со всеми
+// сущностями, которые есть в проекте.
 //todo: добавить возможность опционально выбирать только часть полей для OutputContract
 //todo: добавить возможность изменять поведение на передачу клиентом несуществующих фильтров
 class PresentationBundle extends Bundle
