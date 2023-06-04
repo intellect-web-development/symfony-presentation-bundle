f29590fe887d7da6864a9416bc8c54f70cb10862
alexander <sanyaerin@mail.ru>
2023-06-04 13:41:36 +0300

    chore

--- src/Service/QueryBus/Aggregate/Bus.php
+++ src/Service/QueryBus/Aggregate/Bus.php
@@ -5,22 +5,38 @@ declare(strict_types=1);
 namespace IWD\Symfony\PresentationBundle\Service\QueryBus\Aggregate;
 
 use IWD\Symfony\PresentationBundle\Service\Filter\Fetcher;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class Bus
 {
     public function __construct(
-        private Fetcher $fetcher
+        private Fetcher $fetcher,
+        private AuthorizationCheckerInterface $authorizationChecker,
     ) {
     }
 
-    public function query(Query $actionContext): object
+    public function query(Query $query): object
     {
         return $this->fetcher->getById(
-            context: ($this->fetcher->createContext($actionContext->targetEntityClass))->addFilters($actionContext->filters),
-            id: $actionContext->aggregateId,
-            eager: $actionContext->eager,
-            hints: $actionContext->hints
+            context: ($this->fetcher->createContext($query->targetEntityClass))->addFilters($query->filters),
+            id: $query->aggregateId,
+            eager: $query->eager,
+            hints: $query->hints
         );
+    }
+
+    public function grantedQuery(
+        Query $query,
+        string $grantedAttribute,
+        string $accessDeniedMessage = 'Access Denied.'
+    ): object {
+        $entity = $this->query($query);
+        if (!$this->authorizationChecker->isGranted($grantedAttribute, $entity)) {
+            throw new AccessDeniedException($accessDeniedMessage);
+        }
+
+        return $entity;
     }
 
     public function getRelationPlan(Query $actionContext): array
