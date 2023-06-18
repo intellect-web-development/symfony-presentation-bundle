6b30a7f4b352a7286edf6ebb08d1e7c9536f416f
alexander <sanyaerin@mail.ru>
2023-06-18 16:16:01 +0300

    chore

---
+++ src/Service/CliContractResolver.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IWD\Symfony\PresentationBundle\Service;
+
+use IWD\Symfony\PresentationBundle\Interfaces\InputContractInterface;
+use IWD\Symfony\PresentationBundle\Service\Factory\Interfaces\InputContractFactoryInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+class CliContractResolver
+{
+    public function __construct(
+        private readonly InputContractFactoryInterface $inputContractResolver,
+    ) {
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param class-string<InputContractInterface> $contractClass
+     * @return InputContractInterface
+     */
+    public function resolve(InputInterface $input, string $contractClass): InputContractInterface
+    {
+        /** @var array<string, string> $payload */
+        $payload = array_merge(
+            $input->getOptions(),
+            $input->getArguments(),
+        );
+
+        return $this->inputContractResolver->resolve(
+            $contractClass,
+            $payload
+        );
+    }
+}
